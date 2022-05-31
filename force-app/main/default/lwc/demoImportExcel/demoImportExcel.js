db85534e1c00022762e20751dd3665a675058a3a
linhpq <phamquanglinh@luvina.net>
2022-05-31 16:41:35 +0700

    commit

--- force-app/main/default/lwc/demoImportExcel/demoImportExcel.js
+++ force-app/main/default/lwc/demoImportExcel/demoImportExcel.js
@@ -3,7 +3,6 @@ import { LightningElement, track, wire } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import { loadScript } from 'lightning/platformResourceLoader';
 import lpqresource from "@salesforce/resourceUrl/lpqresource";
-import importObjectFromExcel from "@salesforce/apex/ImportExcelDemo.importObjectFromExcel";
 import getSampleFieldsInfo from "@salesforce/apex/ImportExcelDemo.getSampleFieldsInfo";
 
 let XLS = {};
@@ -18,7 +17,6 @@ export default class DemoImportExcel extends LightningElement {
     @track fileXlsxReady = false;
     @track fileXlsxLoading = false;
     @track fileContent = null;
-    @track allCountry = [];
     @track isInImportProcess = false;
     @track isImportSuccess = false;
     @track excelHeader;
@@ -52,22 +50,6 @@ export default class DemoImportExcel extends LightningElement {
             .catch(() => {
                 console.log("loading SheetJS library failue!");
             });
-    }
-
-     /**
-     * Render call back
-     */
-      renderedCallback() {
-        if (this.currentPage === 1) {
-            this.disableBack = true;
-        } else {
-            this.disableBack = false;
-        }
-        if (this.currentPage === this.totalPageInList) {
-            this.disableNext = true;
-        } else {
-            this.disableNext = false;
-        }
     }
 
     // Import xlsx
@@ -158,91 +140,25 @@ export default class DemoImportExcel extends LightningElement {
 
 
     /**
-     * Handle when click button import excel
+     * Handle when click button import excel, when click validate file
      */
-    async importExcelHandle() {
+    validateExcelHanle() {
         // Validate FrontEnd
-        // this.invalidExcel = false;
-        // let startTimeValidate = performance.now();
-        // let validateResult = this.validateExcelInput();
-        // // Handle when validate frontend error
-        // if (!validateResult.valid) {
-        //     // export excel
-        //     // console.log(validateResult.errors);
-        //     this.invalidExcel = true;
-        //     this.showExcelValidateError(validateResult.errors);
-        //     console.log(`Validate frontent took ${performance.now() - startTimeValidate}`);
-        //     return;
-        // } 
+        this.invalidExcel = false;
+        let startTimeValidate = performance.now();
+        let validateResult = this.validateExcelInput();
+        // Handle when validate frontend error
+        if (!validateResult.valid) {
+            // export excel
+            // console.log(validateResult.errors);
+            this.invalidExcel = true;
+            this.showExcelValidateError(validateResult.errors);
+            console.log(`Validate frontent took ${performance.now() - startTimeValidate}`);
+            return;
+        } 
         console.log(
             "Size Object: " + this.roughSizeOfObject(this.xlsxImportData)
         );
-        let startTime = performance.now();
-        const BLOCK_SIZE = 10000;
-        console.log(`Block size: ${BLOCK_SIZE}`);
-        this.fileXlsxLoading = true;
-
-        importObjectFromExcel({
-            listData: this.xlsxImportData,
-            startIndex: 0,
-            headers: [...this.excelHeader]
-        })
-            .then((data) => {
-                if (data.success) {
-                    console.log(data);
-                    this.isImportSuccess = true;
-                    this.fileXlsxLoading = false;
-                    // Refresh datatable
-                    console.log('Mùi và Toàn thật tuyệt vời!');
-                    this.template.querySelector('c-data-table-demo').refreshData();
-                } else {
-                    // Gen file excel
-                    this.isImportSuccess = false;
-                    this.invalidExcel = true;
-                    this.fileXlsxLoading = false;
-                    this.showExcelValidateError(this.getListErrorsValidate(data));
-                }
-                console.log (`Total time: ${performance.now() - startTime}`); 
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-          
-    }
-
-    /**
-     * Function caculate size of object
-     * @param {*} object object to caculate size
-     * @returns {Number} size of object in bytes 
-     */
-    roughSizeOfObject(object) {
-        var objectList = [];
-        var stack = [object];
-        var bytes = 0;
-
-        while (stack.length) {
-            let value = stack.pop();
-
-            if (typeof value === "boolean") {
-                bytes += 4;
-            } else if (typeof value === "string") {
-                bytes += value.length * 2;
-            } else if (typeof value === "number") {
-                bytes += 8;
-            } else if (
-                typeof value === "object" &&
-                objectList.indexOf(value) === -1
-            ) {
-                objectList.push(value);
-
-                // eslint-disable-next-line guard-for-in
-                for (let i in value) {
-                    stack.push(value[i]);
-                }
-            }
-        }
-        return bytes;
-    
     }
 
     /**
@@ -283,7 +199,6 @@ export default class DemoImportExcel extends LightningElement {
         let rowValid = true;
         let message = {};
         let invalidColumns = [];
-        
         for (let i = 0; i < row.length; i++) {
             let cellValidateError = this.validateCell(row[i], types[i]);
             if (cellValidateError != null) {
@@ -292,11 +207,9 @@ export default class DemoImportExcel extends LightningElement {
                 invalidColumns.push(i);
             }
         }
-
         if(rowValid) {
             return null;
         }
-
         return {
             invalidColumns: invalidColumns,
             message: JSON.stringify(message)
@@ -314,12 +227,10 @@ export default class DemoImportExcel extends LightningElement {
             if (!value && type.isRequired) {
                 return 'Must not empty';
             }
-    
             // type demo : String, number
             if (value && (type.type === 'NUMBER' && isNaN(+value) || (type.type === 'STRING' && typeof value !== 'string'))) {
                 return 'Type invalid';
             }
-    
             // length
             if ((value+'').length > type.length) {
                 return 'Length error';
@@ -361,7 +272,7 @@ export default class DemoImportExcel extends LightningElement {
             for (let i = 0; i < header.length; i++) {
                 let temp = {
                     type: String,
-                    value: rowDataTemp[i]+ ''
+                    value: rowDataTemp[i] ? rowDataTemp[i] + '' : ''
                 };
                 rowData.push(temp);
             }
@@ -379,57 +290,4 @@ export default class DemoImportExcel extends LightningElement {
             fileName: 'file.xlsx'
         })
     }
-
-    /**
-     * Create errors object from apex response. For creating file excel with error infomation.
-     * @param {*} data 
-     * @returns 
-     */
-    getListErrorsValidate(data) {
-        let mapOfFieldInfo = this.getMapOfFieldByHeader([...this.fieldsInfo], [...this.excelHeader]);
-        let mapErrors = new Map();
-        for(const [key, value] of Object.entries(data.saveResults)) {
-            let message = "";
-            let invalidColumns = [];
-            value.errors.forEach(error => {
-                error.fields.forEach(field =>{
-                    let fieldName = field.toLowerCase();
-                    let col = mapOfFieldInfo.has(fieldName) && mapOfFieldInfo.get(fieldName) ? mapOfFieldInfo.get(fieldName) : undefined;
-                    if (col) {
-                        invalidColumns.push(col);
-                    }
-                });
-                message += error.message + ',';
-            });
-            mapErrors.set(parseInt(key, 10), {
-                invalidColumns: invalidColumns,
-                message: message
-            })
-        }
-        return mapErrors;
-    }
-
-    /**
-     * Mapping excel header to Apex SObject field.
-     * @param {*} fieldsInfo 
-     * @param {*} headers 
-     * @returns {Map} map of FieldName to index in header. Ex: "field_101" => 2
-     */
-    getMapOfFieldByHeader(fieldsInfo, headers) {
-        let map = new Map();
-        fieldsInfo.forEach((info) => {
-            let key = info.fieldName;
-            let value;
-            headers.some((header, indexHeader) => {
-                if (info.fieldLabel === header) {
-                    value = indexHeader;
-                    return true;
-                }
-                return false;
-            });
-            map.set(key, value);
-        });
-        return map;
-    }
-
 }
